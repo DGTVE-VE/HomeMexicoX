e8c3c43f56e13e5dbb8a300c468e8799bfdb8ad8
Israel Toledo <j.israel.toledo@gmail.com>
2016-05-12 14:04:54 -0500

    Commit inicial

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+use DB;
+
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+    
+    public function index() {
+        
+        $cursosTodos = DB::select("select *from course_name where course_id is not null and trim(course_id)!=''");
+        
+        return view('home',['cursos'=>$cursosTodos]);
+    }
+}
