1bfbbdf8d328a9b7e2037e628c7b2fa665c79b7a
Sonia Martinez <soniamartinezctr@gmail.com>
2016-12-13 15:25:35 -0600

    quitar imágenes de slider para las pruebas

--- resources/views/viewHome2017/banner.blade.php
+++ resources/views/viewHome2017/banner.blade.php
@@ -1,18 +1,10 @@
 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
   </ol>
   <div class="carousel-inner" role="listbox">
     <div class="carousel-item active">
       <img src="{{'img/banner_mexicox.png'}}" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img src="{{'img/banner_mexicox.png'}}" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img src="{{'img/banner_mexicox.png'}}" alt="Third slide">
     </div>
   </div>
   <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
