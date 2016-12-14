2901e0e69b50444cc8a8f6763d0147441b4fc41f
Crapaud <renecrapaud@gmail.com>
2016-12-14 10:47:51 -0600

    Banner. Ajuste en elementos que se muestran en banner.

--- resources/views/home2017.blade.php
+++ resources/views/home2017.blade.php
@@ -9,14 +9,16 @@ and open the template in the editor.
         <title>@yield('titleMexicoX','MéxicoX')</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!--Bootstrap-->
         
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/cerulean/bootstrap.min.css" rel="stylesheet" type="text/css"/>        
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <!--Javascript-->
         
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js" type="text/javascript"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js" type="text/javascript"></script>
+        
+        <!--Bootstrap-->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
         <script src="{{ asset('js/efectos.js') }}"></script>
         <!--Fuentes-->
         <link href='https://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
--- resources/views/viewHome2017/banner.blade.php
+++ resources/views/viewHome2017/banner.blade.php
@@ -1,9 +1,17 @@
 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
   </ol>
   <div class="carousel-inner" role="listbox">
-    <div class="carousel-item active">
+    <div class="item active">
+      <img src="{{'img/banner_mexicox.png'}}" alt="First slide">
+    </div>
+    <div class="item">
+      <img src="{{'img/banner_mexicox.png'}}" alt="First slide">
+    </div>
+    <div class="item">
       <img src="{{'img/banner_mexicox.png'}}" alt="First slide">
     </div>
   </div>
@@ -17,7 +25,7 @@
   </a>
 </div>
 <script>
-    $('.carousel').carousel({
+    /*$('.carousel').carousel({
   interval: 2000
-})
+})*/
 </script>
