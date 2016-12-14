9b314c3c24946d6a9e192b929bfb6b6185c890a7
Crapaud <renecrapaud@gmail.com>
2016-12-14 11:39:56 -0600

    Menu encabezado. Cambio de color de texto de las opciones del menú. Ajuste de tamaño del logo de méxico x para que ajuste dentro de la barra.

--- resources/views/viewHome2017/encabezadoMexicoX.blade.php
+++ resources/views/viewHome2017/encabezadoMexicoX.blade.php
@@ -1,22 +1,22 @@
 <nav class="navbar navbar-default navbar-fixed-top morado">
     <div class="container-fluid">
-        <div class="navbar-header">
+        <div class="navbar-header" style="margin-left:20px;">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#"><img src="img/logo.png"></a>
+            <a class="navbar-brand" href="#" style="padding:0px;"><img src="img/logo.png" style="height:95%;"></a>
         </div>
         <div id="navbar" class="collapse navbar-collapse pull-right">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="http://mx.televisioneducativa.gob.mx/register?next=%2Fdashboard">registrarse</a></li>
-                <li><a href="http://mx.televisioneducativa.gob.mx/login">Iniciar Sesión</a></li>
-                <li><a href="#">FAQs</a></li>
-                <li><a href="#">Nosotros</a></li>
+                <li><a href="http://mx.televisioneducativa.gob.mx/login" class="txtMenu">Iniciar Sesión</a></li>
+                <li><a href="#" class="txtMenu">FAQs</a></li>
+                <li><a href="#" class="txtMenu">Nosotros</a></li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cursos <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle txtMenu" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cursos <span class="caret"></span></a>
                     <ul class="dropdown-menu">
                     {{--*/ $cont = 1; /*--}}
                         @foreach($categorias as $categoria)
