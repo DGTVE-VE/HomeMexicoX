f07763afe73bc719612c959006d2ed58e1434c80
Crapaud <renecrapaud@gmail.com>
2017-01-17 14:02:37 -0600

    Sección Pie. Cambio de clases para que se ajuste a la pantalla de forma responsiva.

--- resources/views/viewHome2017/pieMexicoX.blade.php
+++ resources/views/viewHome2017/pieMexicoX.blade.php
@@ -9,28 +9,31 @@
     <li class="col-md-1 col-sm-3 col-xs-6 col-lg-1"><a href="http://mx.mexicox.gob.mx/about" style="color: black;"><strong>Nosotros</strong></a></li>
     <li class="col-md-7 col-lg-7 hidden-sm hidden-xs"></li>
 </ul>  
-<div class="col-md-1 col-sm-4 col-xs-6 col-lg-1">
+<div class="col-md-2 col-sm-4 col-xs-6 col-lg-1">
     <a href="http://www.gob.mx/sep">
         <img src="img/sep.png" alt="Secretaría de Educación Pública" width="100">
     </a>
 </div>
-<div class="col-md-1 col-sm-4 col-xs-6 col-lg-1">
+<div class="col-md-2 col-sm-4 col-xs-6 col-lg-1">
     <a href="https://www.gob.mx/mexicodigital/">
         <img src="img/mexico_digital.png" alt="México Digital" width="100">
     </a>
 </div>
-<div class="col-md-1 col-sm-4 col-xs-6 col-lg-1">
+<div class="visible-xs col-xs-12" style="padding:10px;"></div>
+<div class="col-md-2 col-sm-4 col-xs-6 col-lg-1">
     <a href="http://www.televisioneducativa.gob.mx/">
         <img src="img/dgtve.png" alt="Televisión Educativa" width="100">
     </a>
 </div>
-<div class="col-md-1 col-sm-4 col-xs-6 col-lg-1">
+<div class="visible-sm col-sm-12" style="padding:10px;"></div>
+<div class="col-md-2 col-sm-4 col-xs-6 col-lg-1">
     <a href="http://mexicox.gob.mx/">
         <img src="img/mexicox.png" alt="MéxicoX" width="100">
     </a>
 </div>
-<div class="col-md-6  hidden-sm col-xs-2 col-lg-6"></div>
-<div class="col-md-1  hidden-sm hidden-xs col-lg-1">
+<div class="visible-xs col-xs-12" style="padding:10px;"></div>
+<div class="hidden-md  hidden-sm hidden-xs col-lg-6"></div>
+<div class="col-md-2  col-sm-4 col-xs-6 col-lg-1">
     <a href="http://open.edx.org">
         <img src="https://files.edx.org/openedx-logos/edx-openedx-logo-tag.png" alt="Con la tecnología de Open edX" width="100">
     </a>
@@ -40,7 +43,7 @@
         <img src="img/logo-swis.png" alt="Premio WSIS" width="100">
     </a>
 </div>
-<div class="col-md-12">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
     <br>
     <p class="text-muted col-md-12 col-sm-12" style="font-size: 10px;">© MéxicoX. Todos los derechos reservados, excepto cuando se indique. EdX, Open edX, y los logos de edX y Open edX son marcas registradas de edX Inc.</p>        
 	<a href="http://mx.mexicox.gob.mx/privacy" style="color: red; font-size: 10px;" aria-expanded="false">Política de privacidad </a> <span style="padding-right:10px; padding-left:10px;"> - </span>
