b957e84abb292d8341bc6c67007fb581df6e6d42
Crapaud <renecrapaud@gmail.com>
2017-02-09 16:18:48 -0600

    Lista de instituciones. Cambio de elemento href por parrafo en opciones de la lista. Cambio en estilos en mouse enter y mouse out.

--- resources/views/viewHome2017/muestraInstituciones.blade.php
+++ resources/views/viewHome2017/muestraInstituciones.blade.php
@@ -15,41 +15,63 @@
 /*        width: 10px !important;
         border: 1px solid black !important;*/
     }
+    .manoApunta{
+        cursor:pointer;
+        z-index:2;
+        padding-right:15px;
+        padding-left:15px;
+    }
+    .opcionOscura{
+        background-color:#503F7C;
+        color:white;
+    }
 </style>
 <link rel="stylesheet" href="{{asset('css/muestraInstituciones.css')}}"/>
 <div class="dropdown" onfocusout="cierraLista()">
     <button id="myBtn" class="dropbtn redondeaDiv">Institución<span class="caret"></span></button>
     <div id="myDropdown" class="dropdown-content redondeaDiv altoListaInst">
+        {{--*/ $i=1 /*--}}
         @foreach($instituciones as $institucion)    
-        <a href="#" onclick="ocultaLista('{{$institucion->institucion}}')">{{$institucion->nombre_institucion}}</a>
+            <p id="opInstitu{{$i}}" class="manoApunta" onclick="ocultaLista('{{$institucion->institucion}}')" onmouseenter="resaltaOpcion(this.id)" onmouseleave="opcionNormal(this.id)">
+                {{$institucion->nombre_institucion}}</p>
+            {{--*/$i++/*--}}
         @endforeach
     </div>
 </div>
 <script>
+    function resaltaOpcion(opcion){
+        idOpinst = "#" + opcion;
+        $(idOpinst).addClass("opcionOscura");
+    }
+    function opcionNormal(opcion){
+        idOpinst = "#" + opcion;
+        $(idOpinst).removeClass("opcionOscura");
+    }
     document.getElementById("myBtn").onclick = function() {muestraLista()};
     function muestraLista() {
         document.getElementById("myDropdown").classList.toggle("show");
     }
     function cierraLista(){
-        setTimeout(function(){ $('#myDropdown').removeClass("show"); },100);
+        setTimeout(function(){ $('#myDropdown').removeClass("show"); },250);
     }
     function ocultaLista(institucion) {
-        $('#myDropdown').removeClass("show");
         $.ajax({
         method: "POST",
                 url: "{{url('Home2017/cursosInstitucion')}}",
                 data: {
-                cat: "{{$categoria}}",
-                        imparte: institucion,
-                        _token: "{{csrf_token()}}"
+                    cat: "{{$categoria}}",
+                    imparte: institucion,
+                    _token: "{{csrf_token()}}"
                 },
                 error: function (ts) {
                 console.log(ts.responseText);
+                $("#contenedorCursos").empty();
                 }
         })
         .done(function (msg) {
-        $("#contenedorCursos").empty();
-                $("#contenedorCursos").append(msg)
+            $("#contenedorCursos").empty();
+            $("#contenedorCursos").append(msg)
         });
+        $('#myDropdown').removeClass("show");
     }
 </script>
