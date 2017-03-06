a2b76904f0b82e2ab6b56458c967d43393711995
Sonia Martinez <soniamartinezctr@gmail.com>
2017-03-06 16:16:14 -0600

    Subir 3  banner

--- resources/views/viewHome2017/banner.blade.php
+++ resources/views/viewHome2017/banner.blade.php
@@ -1,37 +1,46 @@
 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="margin-top: 60px;">
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <!--<li data-target="#carousel-example-generic" data-slide-to="1"></li>-->
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-  </ol>
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-        <img src="{{'img/bannerFront/comoentenderelpersupuesto.jpg'}}"  alt="" class="imgBanner">
-    </div>      
-<!--    <div class="item">
-      <img src="{{'img/bannerFront/comosonnuestrasescuelas.jpg'}}" alt="" class="imgBanner">
-    </div>-->
-    <div class="item">
-      <img src="{{'img/bannerFront/informaciongeoespacial.jpg'}}" alt="" class="imgBanner">
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="{{'img/bannerFront/comoentenderelpersupuesto.jpg'}}"  alt="" class="imgBanner">
+        </div>      
+        <div class="item">
+            <img src="{{'img/bannerFront/informaciongeoespacial.jpg'}}" alt="" class="imgBanner">
+        </div>
+        <div class="item">
+            <img src="{{'img/bannerFront/lahistoriadelavidacotidiana.jpg'}}" alt="" class="imgBanner">
+        </div>
+        <div class="item">
+            <img src="{{'img/bannerFront/algebralineal2daparte.jpg'}}" alt="" class="imgBanner">
+        </div>
+        <div class="item">
+            <img src="{{'img/bannerFront/calculodiferencial2daparte.jpg'}}" alt="" class="imgBanner">
+        </div>
+        <div class="item">
+            <img src="{{'img/bannerFront/introeducacionfinanciera.jpg'}}" alt="" class="imgBanner">
+        </div>
+
     </div>
-    <div class="item">
-      <img src="{{'img/bannerFront/lahistoriadelavidacotidiana.jpg'}}" alt="" class="imgBanner">
-    </div>
-  </div>
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="icon-prev" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="icon-next" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>    
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="icon-prev" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="icon-next" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>    
 </div>
 <style>
-	.imgBanner{
-		width:100%;
-      	}
+    .imgBanner{
+        width:100%;
+    }
 </style>
 <script>
     $('.carousel').carousel({
