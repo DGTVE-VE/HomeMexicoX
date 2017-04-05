716f6e9adbce0d6197a93e4d7babdfc7c9fef722
Sonia Martinez <soniamartinezctr@gmail.com>
2017-04-05 16:22:43 -0500

    Agregar modal para servicio social de Méxicox

--- resources/views/viewHome2017/mexicox.blade.php
+++ resources/views/viewHome2017/mexicox.blade.php
@@ -6,27 +6,25 @@ MéxicoX
 @include('viewHome2017.encabezadoMexicoX')
 @endsection
 @section('cuerpoMexicoX')
-<!--<div class="modal fade" id="modalInf" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
-   <div class="modal-dialog">
-      <div class="modal-content">
-         <div class="modal-header text-info">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h3 class="text-center">¡Aviso Importante!</h3>
-     </div>
-         <div class="modal-body">
-             <h4 class="text-justify">MéxicoX no colabora ni participa de ninguna manera con el formulario:</h4>
-            <img  src="img/movo.png" class="img-responsive center-block" style="width: 350px; height: 430px;">            
-            <h6 class="text-justify">La cual es una página apócrifa que está recolectando datos de usuarios SIN NUESTRO CONSENTIMIENTO.
-Por favor NO ingreses tus datos a esa página ya que no nos hacemos responsables del uso que el usuario que creó ese formulario haga de ellos.
-Para tomar cursos en la plataforma de México X sólo tienes que registrarte en: 
-mx.mexicox.gob.mx/register</h6>
-     </div>
-         <div class="modal-footer">
-        <a href="#" data-dismiss="modal" class="btn btn-danger">Cerrar</a>
-     </div>
-      </div>
-   </div>
-</div>-->
+<div class="modal fade" id="modalInf" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header text-info" style="background-image: url('img/encabezado.jpg')">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            </div>
+            <h2 class="text-center">Servicio Social</h2>
+            <div class="modal-body modal-sm"   style="background-image: url('img/fondo1.png'); width: 100%;">            
+                <h3 class="text-justify">Si deseas realizar tu servicio social o prácticas profesionales, MéxicoX es el lugar para ti.
+                    Necesitamos personas proactivas, con entusiasmo y ganas de formar parte de un gran equipo.
+                </h3>
+                <h4 class="text-center">Para mayores Informes puedes consultar <a href="http://mx.mexicox.gob.mx/donate">  Informes Servicio Social</a></h4>
+            </div>
+            <div class="modal-footer"  style="background: url('img/pie.png') no-repeat; width: 100%;">
+                <br><br><br><a href="#" data-dismiss="modal" class="btn btn-danger center-block">Cerrar</a><br>
+            </div>
+        </div>
+    </div>
+</div>
 @include('viewHome2017.banner')
 <div class="barraGuia col-lg-12 col-md-12 col-sm-12 col-xs-12">
     <div class="col-lg-1 col-md-2 col-sm-2 col-xs-12" >
@@ -58,12 +56,12 @@ llenaCursos("{{url('Home2017/filtrarCurso/0/1')}}", "{{csrf_token()}}", 1);
 /*  *****   Llenar lista de instituciones   *****   */
 llenaInstituciones("{{url('Home2017/obtenerInstitucion/0')}}", "{{csrf_token()}}");
 </script>
-<!--<script>
-   $(document).ready(function()
-   {
-      $("#modalInf").modal("show");
-   });
-</script>-->
+<script>
+    $(document).ready(function ()
+    {
+        $("#modalInf").modal("show");
+    });
+</script>
 @endsection
 @section('pieMexicoX')
 @include('viewHome2017.pieMexicoX')
